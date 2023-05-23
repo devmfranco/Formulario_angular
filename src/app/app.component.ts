d920fffc566b25c12a21652a6f1998d19fb3754b
Michael Rafael Franco Rodriguez <michael.franco@micm.gob.do>
2023-05-23 07:52:33 -0300

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Formulario_angular';
+  nombreCompleto: string | undefined;
+  viceMinisterioDireccion: string | undefined;
+  ubicacionEspecifica: | undefined;
+  numeroContacto:| undefined;
+  ayudaNecesaria: string | undefined;
+  tipoServicio: string| undefined;
+
+  onSubmit() {
+    // Aquí puedes realizar acciones con los datos del formulario
+    console.log('Formulario enviado');
+    console.log('Nombre completo: ' + this.nombreCompleto);
+    console.log('Vice-ministerio o dirección: ' + this.viceMinisterioDireccion);
+    console.log('Ubicación específica: ' + this.ubicacionEspecifica);
+    console.log('Número de contacto o extensión: ' + this.numeroContacto);
+    console.log('Ayuda necesaria: ' + this.ayudaNecesaria);
+    console.log('Tipo de servicio: ' + this.tipoServicio);
+  }
 }
